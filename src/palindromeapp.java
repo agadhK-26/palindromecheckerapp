051b57b3d7f06848e7804acd02e95c477594c5a7
agadhk-26 <ik7854@srmist.edu.in>
2026-02-25 15:47:37 +0530

    START

---
+++ src/palindromeapp.java
@@ -0,0 +1,9 @@
+public class palindromeapp{
+    public static void main(String args[]){
+        int n = 1;
+        String a = "system initialized successfully";
+        System.out.println("welcome to palindrome checker management system");
+        System.out.println("versiom :"+n);
+        System.out.println(a);
+    }
+}
